c4bcbe1626bae4bc3acc73b90b3f6debe025ba7d
thiruna31 <plthirunavukkarasu01@gmail.com>
2025-10-07 18:33:05 -0400

    page 3

---
+++ lib/pages/end_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EndPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          "🕷️ The End — Happy Halloween! 🕷️",
+          style: TextStyle(fontSize: 26, color: const Color.fromARGB(255, 222, 61, 250)),
+        ),
+      ),
+    );
+  }
+}
